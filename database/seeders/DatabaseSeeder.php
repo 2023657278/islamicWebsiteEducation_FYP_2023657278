9e5db691cfc9c04fb5e0dfa7d23811153cc9d29c
AgiruL <amirul10.0063@gmail.com>
2026-06-03 05:08:43 +0800

    seeder class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,22 +2,39 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-
+use App\Models\User;
+use App\Models\Day;
+use Illuminate\Support\Facades\Hash;
 class DatabaseSeeder extends Seeder
 {
     /**
      * Seed the application's database.
      */
-    public function run(): void
+    public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        // 1. Seed Days
+       $day_seed = [
+            ['id'=>'1','day_name'=>'Monday',],
+            ['id'=>'2','day_name'=>'Tuesday'],
+            ['id'=>'3','day_name'=>'Wednesday'],
+            ['id'=>'4','day_name'=>'Thursday'],
+            ['id'=>'5','day_name'=>'Friday'],
+            ['id'=>'6','day_name'=>'Saturday'],
+            ['id'=>'7','day_name'=>'Sunday'],
+            ];
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-       $this->call(DaySeeder::class);
+        foreach ($day_seed as $day_seed)
+        {
+            Day::firstOrCreate($day_seed);
+        }
+
+        // 2. Create Admin Account
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('Password123!'), // Always hash your passwords!
+            'role' => 'admin', // Make sure your User model has 'role' in $fillable
+        ]);
     }
 }
