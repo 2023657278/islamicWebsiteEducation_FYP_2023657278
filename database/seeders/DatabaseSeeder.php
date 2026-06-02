c258d84be20c5271627cd2c5d4377bcc37d47cf7
AgiruL <amirul10.0063@gmail.com>
2026-06-03 05:18:07 +0800

    commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -6,35 +6,25 @@ use Illuminate\Database\Seeder;
 use App\Models\User;
 use App\Models\Day;
 use Illuminate\Support\Facades\Hash;
+
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
     public function run()
     {
-        // 1. Seed Days
-       $day_seed = [
-            ['id'=>'1','day_name'=>'Monday',],
-            ['id'=>'2','day_name'=>'Tuesday'],
-            ['id'=>'3','day_name'=>'Wednesday'],
-            ['id'=>'4','day_name'=>'Thursday'],
-            ['id'=>'5','day_name'=>'Friday'],
-            ['id'=>'6','day_name'=>'Saturday'],
-            ['id'=>'7','day_name'=>'Sunday'],
-            ];
-
-        foreach ($day_seed as $day_seed)
-        {
-            Day::firstOrCreate($day_seed);
+        // 1. Seed Days - Safely
+        $days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+        foreach ($days as $day) {
+            Day::firstOrCreate(['day_name' => $day]);
         }
 
-        // 2. Create Admin Account
-        User::create([
-            'name' => 'Admin',
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('Password123!'), // Always hash your passwords!
-            'role' => 'admin', // Make sure your User model has 'role' in $fillable
-        ]);
+        // 2. Create Admin Account - Safely
+        User::updateOrCreate(
+            ['email' => 'admin@gmail.com'], 
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('Password123!'),
+                'role' => 'admin',
+            ]
+        );
     }
 }
