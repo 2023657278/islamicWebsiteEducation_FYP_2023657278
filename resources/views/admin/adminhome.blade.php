705a4c8c0e0c9b86fa0d7d61a55b071ad33b52a0
AgiruL <amirul10.0063@gmail.com>
2026-06-10 11:02:06 +0800

    dsa

--- resources/views/admin/adminhome.blade.php
+++ resources/views/admin/adminhome.blade.php
@@ -175,11 +175,11 @@
   </nav>
 
   <aside class="main-sidebar elevation-0">
-    <a href="{{ route('admin.dashboard') }}" class="brand-link d-flex align-items-center py-3">
+    <a href="{{ route('admin.dashboard') }}" class="brand-link align-items-center py-3">
       
       <!-- 🟢 CHANGED: Removed restricting classes and added border-radius: 50% with clean dimension constraints -->
       <img src="{{ asset('admin/dist/img/Ilmora.png') }}" alt="Logo" 
-           style="opacity: 0.9; width: 120px; height: 120px; min-width: 60px; border-radius: 50%;">
+           style="opacity: 0.9; width: 60px; height: 120px; min-width: 120px; border-radius: 50%; object-fit: cover;">
       
       <span class="brand-text font-weight-bold text-white ml-3">Ilmora <span style="color: #fbbf24;">PAI</span></span>
     </a>
